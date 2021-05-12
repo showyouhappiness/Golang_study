060e23b0ae9d0b66c0c71f89f4c046214812ea2f
liyongjun <1477676296@qq.com>
2021-05-12 22:54:35 +0800

    接口、行为的定义和实现、扩展和复用

--- src/day4/func_test/func_test.go
+++ src/day4/func_test/func_test.go
@@ -2,8 +2,37 @@ package func_test
 
 import (
 	"fmt"
+	"math/rand"
 	"testing"
+	"time"
 )
+
+func returnMultiValues() (int, int) {
+	return rand.Intn(10), rand.Intn(20)
+}
+
+func timeSpent(inner func(op int) int) func(op int) int {
+	return func(n int) int {
+		start := time.Now()
+		ret := inner(n * 2) //只能写在打印这里，可以查看用时，否则得到的打印时长都为0
+		fmt.Println("time spent:", time.Since(start).Seconds())
+		return ret
+	}
+}
+
+func slowFunc(op int) int {
+	time.Sleep(time.Second * 1)
+	return op * op
+}
+
+func TestFn(t *testing.T) {
+	//a, b := returnMultiValues()
+	//t.Log(a, b)
+	a, _ := returnMultiValues()
+	t.Log(a)
+	tsSf := timeSpent(slowFunc)
+	t.Log(tsSf(10))
+}
 
 func Clear() {
 	fmt.Println("Clear resources")
