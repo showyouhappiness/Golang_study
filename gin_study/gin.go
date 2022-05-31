2caf4108ae2f2729698550b13c55159492ed2745
1477676296@qq.com <1477676296@qq.com>
2022-05-31 23:29:02 +0800

    调用chrome将本地文件展示

--- gin_study/gin.go
+++ gin_study/gin.go
@@ -4,30 +4,48 @@ import (
 	"embed"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"io/fs"
+	"log"
+	"net/http"
 	"os"
 	"os/exec"
 	"os/signal"
+	"strings"
 )
 
+// go:embed frontend/dist/*
 var FS embed.FS
 
 func Gin1() {
 	go func() { // 开启一个goroutine,如果不将gin.Default()放在goroutine中，那么gin.Default()会在主goroutine中执行，会导致主线程阻塞
 		gin.SetMode(gin.DebugMode)
 		r := gin.Default()
-		r.GET("/", func(c *gin.Context) {
-			c.JSON(200, gin.H{
-				"message": "pong",
-			})
+		staticFiles, _ := fs.Sub(FS, "frontend/dist")
+		r.StaticFS("/static", http.FS(staticFiles))
+		r.NoRoute(func(c *gin.Context) {
+			path := c.Request.URL.Path
+			if strings.HasPrefix(path, "/static/") {
+				reader, err := staticFiles.Open("index.html")
+				if err != nil {
+					log.Fatal(err)
+				}
+				defer reader.Close()       // 关闭文件
+				stat, err := reader.Stat() // 获取文件信息
+				if err != nil {
+					log.Fatal(err)
+				}
+				c.DataFromReader(http.StatusOK, stat.Size(), "text/html", reader, nil)
+			} else {
+				c.Status(http.StatusNotFound)
+			}
 		})
 		r.Run(":8080")
-
 	}()
 	chSignal := make(chan os.Signal, 1)   //创建一个信号通道
 	signal.Notify(chSignal, os.Interrupt) //监听指定信号
 	fmt.Println("server start")
 	chromePath := "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe"
-	cmd := exec.Command(chromePath, "--app=http://localhost:8080")
+	cmd := exec.Command(chromePath, "--app=http://127.0.0.1:8080/static/index.html")
 	cmd.Start()
 
 	select {
