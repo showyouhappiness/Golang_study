dbea0e86970a6d13f52278eb23f93792b250cce6
1477676296@qq.com <1477676296@qq.com>
2022-06-04 17:39:29 +0800

    二维码功能完成 文件功能完成

--- go.mod
+++ go.mod
@@ -7,9 +7,11 @@ require (
 	github.com/easierway/concurrent_map v1.0.0
 	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.7.7
+	github.com/google/uuid v1.3.0
 	github.com/gorilla/websocket v1.5.0
 	github.com/jinzhu/gorm v1.9.16
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
+	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
 	github.com/smartystreets/goconvey v1.6.4
 	github.com/stretchr/testify v1.7.0 // indirectgo.sum
 	github.com/zserge/lorca v0.1.10
--- server/controllers/files_controller.go
+++ server/controllers/files_controller.go
@@ -1,1 +1,35 @@
 package controllers
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"log"
+	"net/http"
+	"os"
+	"path"
+	"path/filepath"
+)
+
+func FilesController(c *gin.Context) {
+	file, err := c.FormFile("raw") //获取上传的文件
+	if err != nil {
+		log.Fatal(err)
+	}
+	exe, err := os.Executable() //获取当前执行文件的路径
+	if err != nil {
+		log.Fatal(err)
+	}
+	dir := filepath.Dir(exe)                 //获取当前执行文件的父目录
+	uploads := filepath.Join(dir, "uploads") //拼接路径
+	filename := uuid.New().String()          //生成文件名
+	err = os.MkdirAll(uploads, os.ModePerm)  //创建目录
+	if err != nil {
+		log.Fatal(err)
+	}
+	fullpath := path.Join("uploads", filename+filepath.Ext(file.Filename)) //拼接路径
+	fileErr := c.SaveUploadedFile(file, filepath.Join(dir, fullpath))      //保存文件
+	if fileErr != nil {
+		log.Fatal(fileErr)
+	}
+	c.JSON(http.StatusOK, gin.H{"url": "/" + fullpath}) //返回文件路径
+}
