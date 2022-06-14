13cf25095ec6bca68fd9ee36a0f06630924ca34a
liyongjun <liyongjun@crf-tech.com>
2022-06-14 20:45:46 +0800

    init project "go_bank"

---
+++ src/bankapi/bankapi.go
@@ -0,0 +1,101 @@
+package bankapi
+
+import (
+	"fmt"
+	"go_test/src/bankcore"
+	"net/http"
+	"strconv"
+)
+
+var accounts = map[float64]*bankcore.Account{}
+
+func Login() {
+	accounts[1001] = &bankcore.Account{
+		Customer: bankcore.Customer{
+			Name:    "John",
+			Address: "Los Angeles, California",
+			Phone:   "(213) 555 0147",
+		},
+		Number: 1001,
+	}
+
+}
+
+// 验证账户是否存在
+func Statement(w http.ResponseWriter, req *http.Request) {
+	numberqs := req.URL.Query().Get("number")
+
+	if numberqs == "" {
+		fmt.Fprintf(w, "Account number is missing!")
+		return
+	}
+
+	if number, err := strconv.ParseFloat(numberqs, 64); err != nil {
+		fmt.Fprintf(w, "Invalid account number!")
+	} else {
+		account, ok := accounts[number]
+		if !ok {
+			fmt.Fprintf(w, "Account with number %v can't be found!", number)
+		} else {
+			fmt.Fprintf(w, account.Statement())
+		}
+	}
+}
+
+// 验证存款（验证是否存在该账户，验证要存入的金额是否为有效的数字）
+func Deposit(w http.ResponseWriter, req *http.Request) {
+	numberqs := req.URL.Query().Get("number")
+	amountqs := req.URL.Query().Get("amount")
+
+	if numberqs == "" {
+		fmt.Fprintf(w, "Account number is missing!")
+		return
+	}
+
+	if number, err := strconv.ParseFloat(numberqs, 64); err != nil {
+		fmt.Fprintf(w, "Invalid account number!")
+	} else if amount, err := strconv.ParseFloat(amountqs, 64); err != nil {
+		fmt.Fprintf(w, "Invalid amount number!")
+	} else {
+		account, ok := accounts[number]
+		if !ok {
+			fmt.Fprintf(w, "Account with number %v can't be found!", number)
+		} else {
+			err := account.Deposit(amount)
+			if err != nil {
+				fmt.Fprintf(w, "%v", err)
+			} else {
+				fmt.Fprintf(w, account.Statement())
+			}
+		}
+	}
+}
+
+//  验证取款（验证是否存在该账户，验证要存入的金额是否为有效的数字）
+func Withdraw(w http.ResponseWriter, req *http.Request) {
+	numberqs := req.URL.Query().Get("number")
+	amountqs := req.URL.Query().Get("amount")
+
+	if numberqs == "" {
+		fmt.Fprintf(w, "Account number is missing!")
+		return
+	}
+
+	if number, err := strconv.ParseFloat(numberqs, 64); err != nil {
+		fmt.Fprintf(w, "Invalid account number!")
+	} else if amount, err := strconv.ParseFloat(amountqs, 64); err != nil {
+		fmt.Fprintf(w, "Invalid amount number!")
+	} else {
+		account, ok := accounts[number]
+		if !ok {
+			fmt.Fprintf(w, "Account with number %v can't be found!", number)
+		} else {
+			err := account.Withdraw(amount)
+			if err != nil {
+				fmt.Fprintf(w, "%v", err)
+			} else {
+				fmt.Fprintf(w, account.Statement())
+			}
+		}
+	}
+}
