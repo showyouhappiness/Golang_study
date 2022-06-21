648dd48a3872411b6d27f520e4167881b5c8803d
1477676296@qq.com <1477676296@qq.com>
2022-06-21 23:11:38 +0800

    递归函数  阶乘

--- src/fibonacci/fibonacci_test.go
+++ src/fibonacci/fibonacci_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestFibList(t *testing.T) {
+func TestFibList(t *testing.T) { // 斐波那契数列
 	result := 0
 	for i := 0; i <= 10; i++ {
 		result = fibonacci(i)
@@ -22,7 +22,7 @@ func fibonacci(n int) (res int) {
 	return
 }
 
-func TestExchange(t *testing.T) {
+func TestExchange(t *testing.T) { // 交换两个数
 	a := 1
 	b := 2
 	//tmp := a
@@ -31,4 +31,28 @@ func TestExchange(t *testing.T) {
 	//和python一样
 	a, b = b, a
 	t.Log(a, b)
+}
+
+func TestRecursive(t *testing.T) { // 递归 从10到1
+	reduce(10)
+}
+func reduce(a int) {
+	if a > 0 {
+		print(a)
+		a--
+		reduce(a)
+	}
+}
+
+func TestFactorial(t *testing.T) { // 递归 阶乘
+	i := 15
+	fmt.Printf("%d 的阶乘是 %d\n", i, Factorial(uint64(i)))
+}
+
+func Factorial(n uint64) (result uint64) {
+	if n > 0 {
+		result = n * Factorial(n-1)
+		return result
+	}
+	return 1
 }
