edfa59d7552fd13ba052d7a090e7af699379dc31
1477676296@qq.com <1477676296@qq.com>
2022-06-21 22:45:56 +0800

    递归函数  fibonacci

--- src/error/error_test.go
+++ src/error/error_test.go
@@ -5,21 +5,21 @@ import (
 	"testing"
 )
 
-//func GetFibonacci(n int) []int {
-//	fibList := []int{1, 1}
-//	for i := 2; i < n; i++ {
-//		fibList = append(fibList, fibList[i-2]+fibList[i-1])
-//	}
-//	return fibList
-//}
-//
-//func TestGetFibonacci(t *testing.T) {
-//	t.Log(GetFibonacci(10))
-//}
+func GetFibonacci(n int) []int {
+	fibList := []int{1, 1}
+	for i := 2; i < n; i++ {
+		fibList = append(fibList, fibList[i-2]+fibList[i-1])
+	}
+	return fibList
+}
 
-func GetFibonacci(n int) ([]int, error) {
-	if n < 0 || n > 100 {
-		return nil, errors.New("n should be in[2,100]")
+func TestGetFibonacci(t *testing.T) {
+	t.Log(GetFibonacci(10))
+}
+
+func GetFibonacci1(n int) ([]int, error) {
+	if n < 0 || n > 100 { //错误处理
+		return nil, errors.New("n should be in[2,100]") //返回错误
 	}
 	fibList := []int{1, 1}
 	for i := 2; i < n; i++ {
@@ -28,9 +28,9 @@ func GetFibonacci(n int) ([]int, error) {
 	return fibList, nil
 }
 
-func TestGetFibonacci(t *testing.T) {
+func TestGetFibonacci1(t *testing.T) {
 	//t.Log(GetFibonacci(10))
-	if v, err := GetFibonacci(5); err != nil {
+	if v, err := GetFibonacci1(10); err != nil {
 		t.Error(err)
 	} else {
 		t.Log(v)
--- src/fibonacci/fibonacci_test.go
+++ src/fibonacci/fibonacci_test.go
@@ -6,16 +6,20 @@ import (
 )
 
 func TestFibList(t *testing.T) {
-	numbers2 := [...]int{1, 2, 3, 4, 5, 6}
-	maxIndex2 := len(numbers2) - 1
-	for i, e := range numbers2 {
-		if i == maxIndex2 {
-			numbers2[0] += e
-		} else {
-			numbers2[i+1] += e
-		}
+	result := 0
+	for i := 0; i <= 10; i++ {
+		result = fibonacci(i)
+		fmt.Printf("fibonacci(%d) is: %d\n", i, result)
 	}
-	fmt.Println(numbers2)
+}
+
+func fibonacci(n int) (res int) {
+	if n <= 1 {
+		res = 1
+	} else {
+		res = fibonacci(n-1) + fibonacci(n-2)
+	}
+	return
 }
 
 func TestExchange(t *testing.T) {
