f7f63a0f63edab9a653dad4c75a8c5913d791864
liyongjun <liyongjun@crf-tech.com>
2022-07-04 19:58:07 +0800

    计算与big包

---
+++ src/math_big/math_big_test.go
@@ -0,0 +1,27 @@
+package math_big
+
+import (
+	"fmt"
+	"math"
+	"math/big"
+	"testing"
+)
+
+func TestMathBig(t *testing.T) {
+	// 大整数
+	im := big.NewInt(math.MaxInt64)
+	in := im
+	io := big.NewInt(1956)
+	ip := big.NewInt(1)
+	ip.Mul(im, in).Add(ip, im).Div(ip, io) // ip = im * in + im / io
+	fmt.Printf("Big Int: %v\n", ip)
+
+	// 大有理数
+	rm := big.NewRat(math.MaxInt64, 1956)
+	rn := big.NewRat(-1956, math.MaxInt64)
+	ro := big.NewRat(19, 56)
+	rp := big.NewRat(1111, 2222)
+	rq := big.NewRat(1, 1)
+	rq.Mul(rm, rn).Add(rq, ro).Mul(rq, rp) // rq = rm * rn + ro * rp
+	fmt.Printf("Big Rat: %v\n", rq)
+}
