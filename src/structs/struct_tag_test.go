c0aaba2a0b4ad64e200f919295928cccdfa6c614
1477676296@qq.com <1477676296@qq.com>
2022-07-05 22:21:24 +0800

    带标签的结构体

---
+++ src/structs/struct_tag_test.go
@@ -0,0 +1,26 @@
+package structs
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+type TagType struct { // tags
+	field1 bool   "An important answer"
+	field2 string "The name of the thing"
+	field3 int    "How much there are"
+}
+
+func TestStructTag(t *testing.T) {
+	tt := TagType{true, "Barak Obama", 1}
+	for i := 0; i < 3; i++ {
+		refTag(tt, i)
+	}
+}
+
+func refTag(tt TagType, ix int) {
+	ttType := reflect.TypeOf(tt)
+	ixField := ttType.Field(ix)
+	fmt.Printf("%v\n", ixField.Tag)
+}
