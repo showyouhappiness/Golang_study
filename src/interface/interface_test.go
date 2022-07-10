ad7cbd6ff9e57d71654e752572713171f1fc700f
1477676296@qq.com <1477676296@qq.com>
2022-07-10 17:36:43 +0800

    接口的定义

--- src/interface/interface_test.go
+++ src/interface/interface_test.go
@@ -1,6 +1,9 @@
 package interface_test
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 type Programmer interface {
 	WriteHelloWorld() string
@@ -17,4 +20,51 @@ func TestClient(t *testing.T) {
 	var p Programmer
 	p = new(GoProgrammer)
 	t.Logf(p.WriteHelloWorld())
+}
+
+type Shaper interface {
+	Area() float32
+}
+
+type Square struct {
+	side float32
+}
+
+func (sq *Square) Area() float32 {
+	return sq.side * sq.side
+}
+
+func TestShaper(t *testing.T) {
+	sq1 := new(Square)
+	sq1.side = 5
+
+	var areaIntf Shaper
+	areaIntf = sq1
+	// shorter,without separate declaration:
+	// areaIntf := Shaper(sq1)
+	// or even:
+	// areaIntf := sq1
+	fmt.Printf("The square has area: %f\n", areaIntf.Area())
+}
+
+type Rectangle struct {
+	length, width float32
+}
+
+func (r Rectangle) Area() float32 {
+	return r.length * r.width
+}
+
+func main() {
+
+	r := Rectangle{5, 3} // Area() of Rectangle needs a value
+	q := &Square{5}      // Area() of Square needs a pointer
+	// shapes := []Shaper{Shaper(r), Shaper(q)}
+	// or shorter
+	shapes := []Shaper{r, q}
+	fmt.Println("Looping through shapes for area ...")
+	for n := range shapes {
+		fmt.Println("Shape details: ", shapes[n])
+		fmt.Println("Area of this shape is: ", shapes[n].Area())
+	}
 }
