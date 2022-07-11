a5c3c4306056602e5eca17a33071b13795f42d6a
1477676296@qq.com <1477676296@qq.com>
2022-07-11 21:24:49 +0800

    接口的定义

--- src/interface/simple_interface_test.go
+++ src/interface/simple_interface_test.go
@@ -5,6 +5,11 @@ import (
 	"testing"
 )
 
+type Simpler1 interface {
+	Get() int
+	Put(int)
+}
+
 type Simpler interface {
 	Get() int
 	Set(int)
@@ -31,6 +36,10 @@ func (p *RSimple) Get() int {
 	return p.j
 }
 
+func (p *Simple) Put(u int) {
+	p.i = u
+}
+
 func (p *RSimple) Set(u int) {
 	p.j = u
 }
@@ -48,9 +57,17 @@ func fI(it Simpler) int {
 	}
 }
 
+func fI1(it Simpler1) int {
+	it.Put(5)
+	return it.Get()
+}
+
 func TestSimpleInterface(t *testing.T) {
+
 	var s Simple
 	fmt.Println(fI(&s)) // &s is required because Get() is defined with a receiver type pointer
+	var s1 Simple
+	fmt.Println(fI1(&s1)) // &s is required because Get() is defined with a receiver type pointer
 	var r RSimple
 	fmt.Println(fI(&r))
 }
--- src/interface/type_interface_test.go
+++ src/interface/type_interface_test.go
@@ -1,7 +1,7 @@
 /*
-程序中定义了一个新类型 Circle，它也实现了 Shaper 接口。 if t, ok := areaIntf.(*Square);
+程序中定义了一个新类型 Circle，它也实现了 Shaper1 接口。 if t, ok := areaIntf.(*Square);
 ok 测试 areaIntf 里是否有一个包含 *Square 类型的变量，结果是确定的；然后我们测试它是否包含一个 *Circle 类型的变量，结果是否定的。
-如果忽略 areaIntf.(*Square) 中的 * 号，会导致编译错误：impossible type assertion: Square does not implement Shaper (Area method has pointer receiver)。
+如果忽略 areaIntf.(*Square) 中的 * 号，会导致编译错误：impossible type assertion: Square does not implement Shaper1 (Area method has pointer receiver)。
 链接
 */
 package interface_test
@@ -12,7 +12,7 @@ import (
 	"testing"
 )
 
-type Square struct {
+type Square1 struct {
 	side float32
 }
 
@@ -20,18 +20,18 @@ type Circle struct {
 	radius float32
 }
 
-type Shaper interface {
+type Shaper1 interface {
 	Area() float32
 }
 
 func TestTypeInterface(t *testing.T) {
-	var areaIntf Shaper
-	sq1 := new(Square)
+	var areaIntf Shaper1
+	sq1 := new(Square1)
 	sq1.side = 5
 
 	areaIntf = sq1
-	// Is Square the type of areaIntf?
-	if t, ok := areaIntf.(*Square); ok {
+	// Is Square1 the type of areaIntf?
+	if t, ok := areaIntf.(*Square1); ok {
 		fmt.Printf("The type of areaIntf is: %T\n", t)
 	}
 	if u, ok := areaIntf.(*Circle); ok {
@@ -41,7 +41,7 @@ func TestTypeInterface(t *testing.T) {
 	}
 }
 
-func (sq *Square) Area() float32 {
+func (sq *Square1) Area() float32 {
 	return sq.side * sq.side
 }
 
