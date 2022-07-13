74a9ffe7600df9cf4d5565805e343bd582782601
1477676296@qq.com <1477676296@qq.com>
2022-07-13 23:08:09 +0800

    空接口

---
+++ src/interface/emptyint_switch_test.go
@@ -0,0 +1,34 @@
+package interface_test
+
+import (
+	"fmt"
+	"testing"
+)
+
+type specialString string
+
+var whatIsThis specialString = "hello"
+
+func TypeSwitch() {
+	testFunc := func(any interface{}) {
+		switch v := any.(type) {
+		case bool:
+			fmt.Printf("any %v is a bool type", v)
+		case int:
+			fmt.Printf("any %v is an int type", v)
+		case float32:
+			fmt.Printf("any %v is a float32 type", v)
+		case string:
+			fmt.Printf("any %v is a string type", v)
+		case specialString:
+			fmt.Printf("any %v is a special String!", v)
+		default:
+			fmt.Println("unknown type!")
+		}
+	}
+	testFunc(whatIsThis)
+}
+
+func TestEmptyIntSwitch(t *testing.T) {
+	TypeSwitch()
+}
