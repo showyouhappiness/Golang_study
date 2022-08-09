87130b1d1c4e141b11ecc39ff688b016d31abe1a
1477676296@qq.com <1477676296@qq.com>
2022-08-09 19:24:36 +0800

    使用testing

---
+++ src/error/oddeven_test.go
@@ -0,0 +1,34 @@
+package error
+
+import "testing"
+
+func Even(i int) bool { // Exported function
+	return i%2 == 0
+}
+
+func Odd(i int) bool { // Exported function
+	return i%2 != 0
+}
+
+func TestEven(t *testing.T) {
+	if !Even(10) {
+		t.Log(" 10 must be even!")
+		t.Fail()
+	}
+	if Even(7) {
+		t.Log(" 7 is not even!")
+		t.Fail()
+	}
+
+}
+
+func TestOdd(t *testing.T) {
+	if !Odd(11) {
+		t.Log(" 11 must be odd!")
+		t.Fail()
+	}
+	if Odd(10) {
+		t.Log(" 10 is not odd!")
+		t.Fail()
+	}
+}
