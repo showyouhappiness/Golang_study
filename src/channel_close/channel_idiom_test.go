1f7ae11c33fe5b63301d03f766effa5a70ed4e49
1477676296@qq.com <1477676296@qq.com>
2022-08-25 22:18:23 +0800

    不将通道作为参数传递给协程，而用函数来生成一个通道并返回（工厂角色）；函数内有个匿名函数被协程调用。

---
+++ src/channel_close/channel_idiom_test.go
@@ -0,0 +1,30 @@
+//不将通道作为参数传递给协程，而用函数来生成一个通道并返回（工厂角色）；函数内有个匿名函数被协程调用。
+package channel_close
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestChannelIdiom(t *testing.T) {
+	stream := pump()
+	go suck(stream)
+	time.Sleep(1e9)
+}
+
+func pump() chan int {
+	ch := make(chan int)
+	go func() {
+		for i := 0; ; i++ {
+			ch <- i
+		}
+	}()
+	return ch
+}
+
+func suck(ch chan int) {
+	for {
+		fmt.Println(<-ch)
+	}
+}
