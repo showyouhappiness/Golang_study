0c173746bc61e89ac4cefb4d378abf89db1f1559
1477676296@qq.com <1477676296@qq.com>
2022-08-29 22:09:54 +0800

    实现了一个使用 int 型通道来实现的生成器

---
+++ src/generator/lazy_evaluation_test.go
@@ -0,0 +1,34 @@
+package generator
+
+import (
+	"fmt"
+	"testing"
+)
+
+var resume chan int
+
+func integers() chan int {
+	yield := make(chan int)
+	count := 0
+	go func() {
+		for {
+			yield <- count
+			count += 2
+		}
+	}()
+	return yield
+}
+
+func generateInteger() int {
+	return <-resume
+}
+
+func TestLazyEvaluation(t *testing.T) {
+	resume = integers()
+	//fmt.Println(generateInteger())
+	//fmt.Println(generateInteger())
+	//fmt.Println(generateInteger())
+	for {
+		fmt.Println(generateInteger())
+	}
+}
