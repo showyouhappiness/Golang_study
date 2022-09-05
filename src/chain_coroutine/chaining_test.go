8fb175af073564fee2504c4cb62bf3af1307fcb5
liyongjun <liyongjun@crf-tech.com>
2022-09-05 10:53:16 +0800

    chaining 链式协程

---
+++ src/chain_coroutine/chaining_test.go
@@ -0,0 +1,24 @@
+package chain_coroutine
+
+import (
+	"flag"
+	"fmt"
+	"testing"
+)
+
+var ngoroutine = flag.Int("n", 100000, "how many goroutines")
+
+func f(left, right chan int) { left <- 1 + <-right }
+
+func TestChaining(t *testing.T) {
+	flag.Parse()
+	leftmost := make(chan int)
+	var left, right chan int = nil, leftmost
+	for i := 0; i < *ngoroutine; i++ {
+		left, right = right, make(chan int)
+		go f(left, right)
+	}
+	right <- 0      // bang!
+	x := <-leftmost // wait for completion
+	fmt.Println(x)  // 100000, about 1.5 s
+}
