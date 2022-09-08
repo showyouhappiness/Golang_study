226ef455df602c747bd17881ffeb0fb698cdae74
liyongjun <liyongjun@crf-tech.com>
2022-09-08 18:25:42 +0800

    websocket

---
+++ src/http/websocket/websocket_server_test.go
@@ -0,0 +1,29 @@
+package websocket
+
+import (
+	"fmt"
+	//"github.com/gorilla/websocket"
+	"golang.org/x/net/websocket"
+	"net/http"
+)
+
+func server(ws *websocket.Conn) {
+	fmt.Printf("new connection\n")
+	buf := make([]byte, 100)
+	for {
+		if _, err := ws.Read(buf); err != nil {
+			fmt.Printf("%s", err.Error())
+			break
+		}
+	}
+	fmt.Printf(" => closing connection\n")
+	ws.Close()
+}
+
+func main() {
+	http.Handle("/websocket", websocket.Handler(server))
+	err := http.ListenAndServe(":12345", nil)
+	if err != nil {
+		panic("ListenAndServe: " + err.Error())
+	}
+}
